f839a6bd6c7482a88ae53c1d868d57cce8d76192
Ckogann <ionov.kos@gmail.com>
2025-09-01 13:30:41 +0200

    commented yield

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from selene import browser
 @pytest.fixture(scope="function", autouse=True)
 def open_browser():
     browser.config.base_url = 'https://demoqa.com'
-
-    yield
-
-    browser.quit()
+    #
+    # yield
+    #
+    # browser.quit()
