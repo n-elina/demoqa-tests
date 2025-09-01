6ea3cdf8ef969052a8b7fda2a94136a2487f1cf5
Ckogann <ionov.kos@gmail.com>
2025-09-01 16:26:30 +0200

    added attachments

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,7 +12,7 @@ def setup_browser():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": "128.0",
         "selenoid:options": {"enableVNC": True, "enableVideo": True},
     }
     options.capabilities.update(selenoid_capabilities)
