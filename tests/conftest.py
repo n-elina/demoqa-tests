0ed9270521aef8c89c2b1a4f39423d9166a27d6a
Ckogann <ionov.kos@gmail.com>
2025-09-01 16:21:25 +0200

    added attachments

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
 import pytest
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
+
+from utils import attach
 
 
 @pytest.fixture(scope='function')
@@ -9,7 +12,7 @@ def setup_browser(request):
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "128.0",
+        "browserVersion": "100.0",
         "selenoid:options": {"enableVNC": True, "enableVideo": True},
     }
     options.capabilities.update(selenoid_capabilities)
@@ -21,5 +24,10 @@ def setup_browser(request):
     browser = Browser(Config(driver))
 
     yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
