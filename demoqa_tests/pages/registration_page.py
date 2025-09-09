6fab29adc448917fbe2cdad2d9299ea630740171
Ckogann <ionov.kos@gmail.com>
2025-09-09 07:38:12 +0200

    added attachments

--- demoqa_tests/pages/registration_page.py
+++ demoqa_tests/pages/registration_page.py
@@ -6,7 +6,7 @@ from demoqa_tests.data.user import User
 class RegistrationPage:
 
     def open(self):
-        browser.open('/automation-practice-form')
+        browser.open('https://demoqa.com/automation-practice-form')
         browser.all('[id^=google_ads][id$=container__]').with_(timeout=10).wait_until(
             have.size_greater_than_or_equal(2)
         )
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
 import pytest
 from selene import browser
 
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+# from utils import attach
+
 
 @pytest.fixture(scope="function", autouse=True)
 def open_browser():
@@ -9,3 +13,30 @@ def open_browser():
     yield
 
     browser.quit()
+
+
+# @pytest.fixture(scope='function', autouse=True)
+# def setup_browser():
+#     options = Options()
+#     selenoid_capabilities = {
+#         "browserName": "chrome",
+#         "browserVersion": "128.0",
+#         "selenoid:options": {"enableVNC": True, "enableVideo": True},
+#     }
+#     options.capabilities.update(selenoid_capabilities)
+#     driver = webdriver.Remote(
+#         command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+#         options=options,
+#     )
+#
+#     browser = Browser(Config(driver=driver))
+#     # browser.config.base_url = 'https://demoqa.com'
+#
+#     yield browser
+#
+#     attach.add_screenshot(browser)
+#     # attach.add_logs(browser)
+#     attach.add_html(browser)
+#     attach.add_video(browser)
+#
+#     browser.quit()
