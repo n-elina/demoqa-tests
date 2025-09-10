83f4e7ee29226a8f7b9d977f1fe7e0c41ff19515
Ckogann <ionov.kos@gmail.com>
2025-09-10 18:02:01 +0200

    hid login/password

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
+import os
+
 import pytest
 from selene import Browser, Config
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from utils import attach
+from dotenv import load_dotenv
 
 
 def pytest_addoption(parser):
@@ -10,6 +13,11 @@ def pytest_addoption(parser):
         '--browser_version',
         default='128.0',
     )
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope='function')
@@ -23,8 +31,11 @@ def setup_browser(request):
     }
     options.capabilities.update(selenoid_capabilities)
 
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud / wd / hub",
         options=options,
     )
     browser = Browser(Config(driver=driver))
